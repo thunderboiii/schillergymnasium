9ab95588bbba7ce63c1f3e7bba970243dad1e236
thunderboiii <emil@maple-ink.com>
2023-07-10 20:26:02 +0200

    Safari Update

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,6 +1,22 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('mobile-links')[0];
+const mobileLinks = document.getElementsByClassName('mobile-links')[0];
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle("active");
+    mobileLinks.classList.toggle("active");
 });
+
+document.addEventListener('click', e=> {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]");
+    if(!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+    
+    let currentDropdown
+    if(isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+        if(dropdown === currentDropdown) return
+        dropdown.classList.remove('active');
+    });
+}); 
