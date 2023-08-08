007031082c841e49bd09d997f1cb26715b55a505
thunderboiii <emil@maple-ink.com>
2023-08-08 18:07:04 +0200

    aktuelles

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/scripts');
     eleventyConfig.addPassthroughCopy('./src/admin');
 
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
     return {
         dir: {
             input: "src",
