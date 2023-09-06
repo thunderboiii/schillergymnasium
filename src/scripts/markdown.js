727e2c87dd4aaefb38971638aa82fad95a5e32c0
thunderboiii <emil@maple-ink.com>
2023-09-06 09:42:02 +0200

    idk

--- src/scripts/markdown.js
+++ src/scripts/markdown.js
@@ -4,4 +4,15 @@ const marked = require('marked');
 
 nunjucks.configure({ autoescape: true });
 
-const mdFileContent = fs.readFileSync()
+const mdFileContent = fs.readFileSync('src/_pages/fächer/kunst.md', 'utf-8');
+
+const [frontMatter, content] = mdFileContent.split('---').slice(1);
+
+const htmlContent = marked(content);
+
+const templateData = {
+    frontMatter: YAML.parse (frontMatter.trim()),
+    content: htmlContent,
+};
+
+const html = nunjucks.render('src/_includes/fächer.njk', templateData);
