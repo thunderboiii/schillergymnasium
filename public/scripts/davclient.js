1905bbc3e415e4cf9d45b88d5a751fce1061ee5e
thunderboiii <emil@maple-ink.com>
2023-09-21 17:47:55 +0200

    caldav

---
+++ public/scripts/davclient.js
@@ -0,0 +1,22 @@
+import dav from 'dav';
+
+const xhr = new dav.transport.Basic(
+    new dav.Credentials({
+        username: 'homepage',
+        password: '*pagehome*',
+    })
+);
+
+const client = new dav.Client(xhr);
+
+const calendarUrl = 'https://schiller.ms.de/caldav/+public/calendar';
+
+client.createAccount({
+    server: calendarUrl,
+}).then((account) => {
+    return account.calendarObjects.list();
+}).then ((calendarObjects) => {
+    console.log(calendarObjects);
+}).catch((error) => {
+    console.error(error);
+});
