44c6a48df84e9047d5f9485b792c980d1add98ea
thunderboiii <emil@maple-ink.com>
2023-10-15 19:42:44 +0200

    yolo

---
+++ public/scripts/fächer.js
@@ -0,0 +1,14 @@
+const images = document.getElementsByClassName("fachschaftImage");
+const imgContainer = document.getElementById("fachschaft-gallery");
+
+const imageLength = images.length;
+
+if(length % 3 === 0) {
+    imgContainer.classList.add("3");
+} else if (length % 4 === 0) {
+
+} else if (length % 5 === 0) {
+
+} else {
+    return;
+}
