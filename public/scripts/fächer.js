5400cc8c7fd24b329d0217790c5a3070a124a93e
thunderboiii <emil@maple-ink.com>
2023-10-23 16:22:51 +0200

    style

--- public/scripts/fächer.js
+++ public/scripts/fächer.js
@@ -4,11 +4,11 @@ const imgContainer = document.getElementById("fachschaft-gallery");
 const imageLength = images.length;
 
 if(length % 3 === 0) {
-    imgContainer.classList.add("3");
+    imgContainer.style.width = "calc((2 * 1rem) + (3* 145px))";
 } else if (length % 4 === 0) {
-
+    imgContainer.style.width = "calc((3 * 1rem) + (4* 145px))";
 } else if (length % 5 === 0) {
-
+    imgContainer.style.width = "calc((4 * 1rem) + (5* 145px)) !important";
 } else {
     return; 
 }
