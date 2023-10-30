f64521c81e5dda659e02897d161709f1151d3eb9
thunderboiii <emil@maple-ink.com>
2023-10-30 17:34:09 +0100

    oh oh

--- src/scripts/table.js
+++ src/scripts/table.js
@@ -4,9 +4,17 @@ const tableRoot = document.querySelector("#csvRoot");
 const tableCSV = new tableCons(tableRoot);
 const csvFileInput = document.querySelector("#csvFileInput");
 
-csvFileInput.addEventListener("change", e => {
-    Papa.parse(csvFileInput.files[0], {
-        delimter: ",",
+const csvDataElement = document.getElementById("csvFile");
+const dataAttribute = csvDataElement.getAttribute("data-table");
+const csvFile = dataAttribute;
+
+
+// Add an event listener for the "load" event of the window
+window.addEventListener("load", () => {
+    // Your code to parse the CSV and update the table
+    Papa.parse(csvFile, {
+        download: true,
+        delimiter: "",
         skipEmptyLines: false,
         complete: results => {
             tableCSV.update(results.data.slice(1), results.data[0]);
