1e529fc861838b8e167af27e7f5b22e295b65660
thunderboiii <emil@maple-ink.com>
2023-11-14 17:15:07 +0100

    ugh

--- public/scripts/termine.js
+++ public/scripts/termine.js
@@ -1,9 +1,7 @@
-// import ical
 const ical = require('node-ical');
 
-// use the sync function parseFile() to parse this ics file
 const events = ical.sync.parseFile('https://calendar.google.com/calendar/ical/r59d3v49824ce551us502etp002dkdec%40import.calendar.google.com/public/basic.ics');
-// loop through events and log them
+
 for (const event of Object.values(events)) {
     console.log(
         'Summary: ' + event.summary +
@@ -11,4 +9,4 @@ for (const event of Object.values(events)) {
         '\nStart Date: ' + event.start.toISOString() +
         '\n'
     );
-};
+}
