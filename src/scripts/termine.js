7f894c182971b6beceda55bf92ad44f8b4758b02
thunderboiii <emil@maple-ink.com>
2023-11-14 16:57:58 +0100

    jdijdj2io0r

--- src/scripts/termine.js
+++ src/scripts/termine.js
@@ -1,52 +1,14 @@
-// Define your date range (current date to one week from now)
-const currentDate = new Date();
-const oneWeekLater = new Date();
-oneWeekLater.setDate(currentDate.getDate() + 7);
+// import ical
+const ical = require('node-ical');
 
-// Use a CORS proxy to fetch the iCal data
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const calendarUrl = 'https://calendar.google.com/calendar/ical/r59d3v49824ce551us502etp002dkdec%40import.calendar.google.com/public/basic.ics';
-
-fetch(proxyUrl + calendarUrl)
-    .then(response => response.text())
-    .then(data => {
-        // Parse iCal data to extract events within the date range
-        const jcalData = ICAL.parse(data);
-        const comp = new ICAL.Component(jcalData);
-        const vevents = comp.getAllProperties('vevent');
-        const events = vevents
-            .map(vevent => {
-                const dtstart = vevent.getFirstValue('dtstart').toJSDate();
-                return {
-                    summary: vevent.getFirstValue('summary'),
-                    start: dtstart,
-                };
-            })
-            .filter(event => event.start >= currentDate && event.start <= oneWeekLater);
-
-        // Generate and append HTML for each event
-        const scrollElement = document.querySelector('.scroll');
-        for (const event of events) {
-            const eventHtml = generateEventHTML(event);
-            scrollElement.innerHTML += eventHtml;
-        }
-    });
-
-// Function to generate HTML for an event
-function generateEventHTML(event) {
-    const eventDate = event.start;
-    const day = eventDate.getDate();
-    const month = eventDate.getMonth() + 1;
-    const year = eventDate.getFullYear();
-    const eventTitle = event.summary;
-
-    return `
-        <div class="termin scroll-item">
-            <div class="date">
-                <p class="day">${day}.</p>
-                <p>${month}. ${year}.</p>
-            </div>
-            <h3>${eventTitle}</h3>
-        </div>
-    `;
-}
+// use the sync function parseFile() to parse this ics file
+const events = ical.sync.parseFile('https://calendar.google.com/calendar/ical/r59d3v49824ce551us502etp002dkdec%40import.calendar.google.com/public/basic.ics');
+// loop through events and log them
+for (const event of Object.values(events)) {
+    console.log(
+        'Summary: ' + event.summary +
+        '\nDescription: ' + event.description +
+        '\nStart Date: ' + event.start.toISOString() +
+        '\n'
+    );
+};
