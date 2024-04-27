5a5cde286d225b1ebaba55fead048706294a6f92
thunderboiii <homepage@schiller.ms.de>
2024-04-27 21:47:30 +0200

    Update sendmail.php

--- src/scripts/sendmail.php
+++ src/scripts/sendmail.php
@@ -1,28 +1,20 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(
-        !empty($_POST['name'])
-        && !empty($_POST['email'])
-        && !empty($_POST['message'])
-    ){
-        $vorname = $_POST["vorname"];
-        $nachname = $_POST["nachname"];
-        $email = $_POST["email"];
-        $subject = $_POST["subject"];
-        $message = $_POST["msg"];
+if (isset($_POST['submit'])) {
+    $vorname = $_POST["vorname"];
+    $nachname = $_POST["nachname"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["msg"];
 
 
-        $to = "emil@maple-ink.com";
-        $subject = "[Kontaktformular] ${subject}";
-        $body = "Name: {$vorname} {$nachname}\nEmail: {$email}\Nachricht:\n{$message}";
-        $headers = "From: {$email}";
+    $to = "emil@maple-ink.com";
+    $subject = "[Kontaktformular] {$subject}";
+    $body = "Name: {$vorname} {$nachname}\nEmail: {$email}\Nachricht:\n{$message}";
+    $headers = "From: {$email}";
 
 
-        if (mail($to, $subject, $body, $headers)) {
-            echo "Message sent successfully!";
-        } else {
-            echo "Failed to send message.";
-        }
-    }
+    mail($to, $subject, $body, $headers);
+
+    header("Location: /kontakt");
 }
